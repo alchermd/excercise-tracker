79552d20bf71f643cdf64397e37def98cc938830
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-03-28 20:18:40 +0800

    Implement user creation

--- app.go
+++ app.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -15,7 +21,22 @@ func main() {
 		log.Fatal("$PORT is not set")
 	}
 
+	dns := os.Getenv("DATABASE_URL")
+
+	if dns == "" {
+		log.Fatal("$DATABASE_URL is not set")
+	}
+
+	db, err := sql.Open("mysql", dns)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/api/exercise/new-user", func(w http.ResponseWriter, r *http.Request) {
+		newUserHandler(w, r, db)
+	})
 
 	log.Print("Serving static assets on /assets")
 	fs := http.FileServer(http.Dir("assets/"))
@@ -30,4 +51,49 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	t := template.Must(template.ParseFiles("views/index.html"))
 	t.Execute(w, nil)
+}
+
+func newUserHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	log.Print("Serving " + r.URL.Path)
+	p := getPayload(r)
+	username := p["username"]
+
+	var count int
+	rows, err := db.Query("SELECT COUNT(id) FROM users WHERE username = ?", username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows.Next()
+	rows.Scan(&count)
+
+	if count != 0 {
+		http.Error(w, `{"message": "username already taken"}`, http.StatusConflict)
+		return
+	}
+
+	stmt, err := db.Prepare("INSERT INTO users(username) VALUES(?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(username)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, `{"username": "%s", "_id": "%d"}`, username, id)
+}
+
+func getPayload(r *http.Request) map[string]string {
+	body := make(map[string]string)
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &body)
+
+	return body
 }
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,39 @@
-alert("hello, world!");
+const vm = new Vue({
+	el: "#app",
+	delimiters: ["${", "}"],
+	data: {
+		newUsername: "",
+		response: "",
+	},
+	methods: {
+		createNewUser(e) {
+			e.preventDefault();
+
+			fetch("/api/exercise/new-user", {
+				method: "POST",
+				body: JSON.stringify({
+					username: this.newUsername
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			.then(res => {
+				if (res.ok) {
+					return res.json();	
+				}
+
+				if (res.status === 409) {
+					throw new Error("Username already taken");
+				}
+			})
+			.then(data => {
+				console.log(data);
+				this.response = `User Created! username:${data.username} id: ${data._id}`
+			})
+			.catch(err => {
+				this.response = "Username is already taken!"
+			})
+		}
+	}
+})
